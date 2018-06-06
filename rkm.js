663a3280e47de87d9ac8a085b040d852ae31831c
Joe K. Kim (xGovern Team) <jkk@xgovern.com>
2018-06-06 06:03:45 -0400

    rkm main

--- rkm.js
+++ rkm.js
@@ -1,1 +1,42 @@
-var xgov = require("xgov");
+/*
+//=============================================================================================================
+//
+//		Name        : 	RKM Protocol
+//		Author      : 	Anvay Rane, Joe K. Kim (J.K.K.), Shinsuke Matsumoto
+//		Site		:	recoverkey.io
+//		Version     : 	1.0.0
+//		
+//		!!! Proprietary !!!
+//
+//		© 2018 Potion, all rights reserved.
+//		Unauthorized copying of this file, via any medium is strictly prohibited.
+//		Written by Joe K. Kim <jkk@xgovern.com> and Anvay Rane <anvay.rane@gmail.com>
+//
+//=============================================================================================================
+*/
+
+var is_nodeJS_master, is_nodeJS, is_worker, is_xgov_webnode, beta_mode;
+is_nodeJS = (new Function("try{return this===global;}catch(e){return false;}"))();
+if(is_nodeJS){try{is_worker=require?false:true;}catch(e){is_worker = true;}}
+else{is_worker = (new Function("try{document}catch(e){return true;}"))();}
+is_nodeJS_master = (is_nodeJS && !is_worker);
+//if(is_worker && !is_nodeJS) importScripts("https://s3.amazonaws.com/jvs-static/nacl-fast.min.js");
+
+var time = function(){return Math.floor((new Date).getTime()/1000);}
+var mtime = function(){return (new Date).getTime();}
+var portable_error = function(e){if(typeof e === 'string') e = new Error(e); if(!e) e = new Error; return {mesasge:e.message, stack:e.stack, name:e.name};}
+var sort_object_keys = function(obj){
+	return Object.keys(obj).sort().reduce(function(result,key){result[key] = obj[key]; return result;}, {});
+}
+
+var rkm = (function(){
+
+	var rkm = {};
+
+	try{if(is_nodeJS_master && module) module.exports = rkm;}
+	catch(e){}
+	
+	Object.freeze(rkm);
+    return rkm;
+    
+})();
